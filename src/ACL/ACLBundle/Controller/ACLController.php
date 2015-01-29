9e9deff068fb14dfe2957cbbce544f4980bb8f7f
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-01-29 16:13:28 -0600

    Configuración del proveedor de usuarios

--- src/ACL/ACLBundle/Controller/ACLController.php
+++ src/ACL/ACLBundle/Controller/ACLController.php
@@ -5,12 +5,44 @@ namespace ACL\ACLBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Response;
+use ACL\ACLBundle\Entity\User;
+
 
 class ACLController extends Controller
 {
   public function indexAction($name)
   {
     return $this->render('ACLBundle:Default:index.html.twig', array('name' => $name));
+  }
+
+  public function newAction()
+  {
+    $user = new User();
+
+    $user->setUsername("cristianangulonova");
+
+    $factory = $this->get('security.encoder_factory');
+    $encoder = $factory->getEncoder($user);
+
+    $user->setPassword($encoder->encodePassword('foo', $user->getSalt()));
+    $user->setEmail("cristianangulonova@gmail.com");
+    $user->setIsActive(1);
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($user);
+    $em->flush();
+
+    return new Response('Se ha creado el usuario id: '.$user->getId());
+  }
+
+  public function perfilAction()
+  {
+    $user = $this->get('security.context')->getToken()->getUser();
+
+    $roles = new Roles\Roles();
+
+    return $this->render('ACLBundle:ACL:perfil.html.twig');
   }
 
   public function loginAction(Request $request)

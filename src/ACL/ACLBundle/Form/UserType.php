6e3960760dd0cb8d3486c5d53260753e4699f447
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-01-31 07:37:13 -0600

    Crud User

---
+++ src/ACL/ACLBundle/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ACL\ACLBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('email')
+            ->add('isActive')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ACL\ACLBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'acl_aclbundle_user';
+    }
+}

6199861de351e16d10c8ed984eb252bd679e8634
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-02-04 17:18:00 -0600

    Usuarios y Roles

---
+++ src/ACL/ACLBundle/Form/RolesType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ACL\ACLBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RolesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('role')
+            ->add('role_key')
+            ->add('usuarios', 'entity', array(
+              'class' => 'ACLBundle:Usuarios',
+              'property' => 'username',
+              'expanded' => true,
+              'multiple' => true
+            ))
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ACL\ACLBundle\Entity\Roles'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'acl_aclbundle_roles';
+    }
+}
